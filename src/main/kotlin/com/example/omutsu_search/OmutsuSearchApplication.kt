431b77061cbbf65aefbf9e932157e8ad712d4d38
ytttt-2kab <60835290+ytttt-2kab@users.noreply.github.com>
2022-08-24 11:48:24 +0900

    add projext

---
+++ src/main/kotlin/com/example/omutsu_search/OmutsuSearchApplication.kt
@@ -0,0 +1,11 @@
+package com.example.omutsu_search
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OmutsuSearchApplication
+
+fun main(args: Array<String>) {
+	runApplication<OmutsuSearchApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/omutsu_search/OmutsuSearchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.omutsu_search
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OmutsuSearchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
