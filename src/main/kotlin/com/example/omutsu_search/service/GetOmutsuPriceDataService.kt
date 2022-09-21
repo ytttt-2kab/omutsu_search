df9c925553fc1a84696a6acc6f87e4d82944b41a
ytttt-2kab <60835290+ytttt-2kab@users.noreply.github.com>
2022-09-21 10:53:40 +0900

    Todoコメント追加

---
+++ src/main/kotlin/com/example/omutsu_search/service/GetOmutsuPriceDataService.kt
@@ -0,0 +1,8 @@
+package com.example.omutsu_search.service
+
+// Todo: バッチ処理
+// Todo: 楽天、アマゾンごとの処理作成
+// Todo: brandとかのデータ登録（データ様式見直す）
+interface GetOmutsuPriceDataService {
+    fun getOmutsuPriceData()
+}
